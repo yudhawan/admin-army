78007e68165151d66192fbee731d48af516c864f
Yasha <yudhaashter@gmail.com>
2022-04-26 09:55:55 +0700

    adding carco

--- src/components/Monitoring/MapZone.js
+++ src/components/Monitoring/MapZone.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 let MAPBOX_TOKEN = "pk.eyJ1IjoieXVkaGF3YW4iLCJhIjoiY2wxa2cxZ3h2MDBnNDNqangzaHFuNXpwNyJ9.-NB1Dw2GlDb11fnUD3ZQng"
+// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
 mapboxgl.accessToken=MAPBOX_TOKEN
 function MapZone({long,lat,fullmap}) {
   const [maps,setmaps]=useState(null)
